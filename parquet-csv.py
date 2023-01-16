29007ad6941c859c2d2e770c7c5ed4e13e6bb191
Akshay patidar <akshaypatidar2604@gmail.com>
2023-01-16 18:01:35 +0530

    Update parquet-csv.py

--- parquet-csv.py
+++ parquet-csv.py
@@ -2,11 +2,10 @@ import pandas as pd
 import glob
 import os 
 
-# path = glob.glob(folder_path+'/*.parquet')  # Enter folder path as string
-path = glob.glob("/Users/apple/Desktop/oral"+'/*.parquet')  # Enter folder path as string
+ path = glob.glob(folder_path+'/*.parquet')  # Enter folder path as string
 df = pd.read_parquet(path[0], engine='pyarrow')
 for i in range(1,len(path)):
     df1 = pd.read_parquet(path[i], engine='pyarrow')
     df  = pd.concat([df,df1],axis = 0)
     print(len(df))
-df.to_csv("op1.csv")
+df.to_csv("fileName.csv") # add fileName
